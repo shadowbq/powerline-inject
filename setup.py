96823f9f3573bbd1a9aa4981bda107ae78cb87d0
d2iq-shadowbq <smacgregor@d2iq.com>
2020-01-21 17:09:13 -0500

    fix os path

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ try:
 except ImportError:
    CleanCommand = None
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 cmd_classes = {}
